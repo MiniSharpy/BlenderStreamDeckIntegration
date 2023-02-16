8c150959e0b41ac7b7968c0c2eb6fcf71527462f
MiniSharpy <46828188+MiniSharpy@users.noreply.github.com>
2023-02-16 23:34:48 +0000

    Add exception handling to server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 # A non-blocking server to listen for input messages from within Blender.
+# https://docs.python.org/3/library/asyncio-stream.html#tcp-echo-server-using-streams
 import asyncio, threading, bpy
 
 async def handle_input(reader, writer):
@@ -8,11 +9,15 @@ async def handle_input(reader, writer):
 
     print(f"Received {message!r} from {addr!r}")
 
-    bpy.ops.paint.brush_select(sculpt_tool=message) # Is this safe to run on another thread? It might be better to execute this from the main thread using a queue.
-
-    print("Close the connection")
-    writer.close()
-    await writer.wait_closed()
+    # TODO: Switch the correct tool, seems if transform tools are selected the brush_select won't switch it.
+    try: # Can get an exception if invalid sculpt_tool.
+        bpy.ops.paint.brush_select(sculpt_tool=message) # Is this safe to run on another thread? It might be better to execute this from the main thread using a queue.
+    except TypeError as e:
+        print(e)
+    finally:
+        print("Close the connection")
+        writer.close()
+        await writer.wait_closed()
 
 async def main():
     server = await asyncio.start_server(
